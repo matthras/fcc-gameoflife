814f02846d528261091dc1b33d639427f00ba80d
matthras <matthew.y.p.mack@gmail.com>
2017-10-01 23:54:16 +1100

    added window Width+Height for grid calculations

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import './App.css';
 import Grid from './Grid.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowWidth: window.innerWidth
+      || document.documentElement.clientWidth
+      || document.body.clientWidth,
+      windowHeight: window.innerHeight
+      || document.documentElement.clientHeight
+      || document.body.clientHeight
+    }
+  }
+  // Calculate 
+  componentWillMount() {
+
+  }
   render() {
     return (
       <div className="App">
         <header className="header">
           <h1 className="header-title">Conway's Game Of Life</h1>
         </header>
+        <p>Width of the window is {this.state.windowWidth}</p>
+        <p>Height of the windows is {this.state.windowHeight}</p>
         <Grid />
       </div>
     );
